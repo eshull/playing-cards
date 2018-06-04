4bd6c8127160b3ce9f0ae09ef47dd3de4e9668b8
Eric Shull and Ryan Putman <eric@>
2018-06-04 16:37:28 -0700

    add car script to practice looping

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,7 @@ $(function(){
   var values = ["ace", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "jack", "queen", "king"]
 
     suits.forEach(function(suit){
+
       values.forEach(function(value){
         $("ul").append("<li>" + value + " of " + suit + "</li>");
 
@@ -12,3 +13,18 @@ $(function(){
 
 
   });
+
+  $(function(){
+
+    var cars = ["ford", "dodge", "pontiac", "chevy"]
+    var colors = ["blue", "red", "green", "purple", "yellow"]
+
+      cars.forEach(function(car){
+        colors.forEach(function(color){
+          $("ul").append("<li>" + car + " in " + color + "</li>");
+
+        })
+      });
+
+
+    });
